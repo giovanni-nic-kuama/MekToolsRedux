bbc1b16ed3a8c9a8ed60ed5fb3a64b4dfe550558
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-29 15:42:30 +0100

    feat (App) add DashboardPage.razor

--- MekToolsReduxComponents/Modules/ExportedMekToolsComponentsAssemblies.cs
+++ MekToolsReduxComponents/Modules/ExportedMekToolsComponentsAssemblies.cs
@@ -1,17 +1,12 @@
 ﻿using System.Reflection;
-using MekToolsReduxComponents.Modules.Index.Pages;
 using MekToolsReduxComponents.Modules.Main.Pages;
 
 namespace MekToolsReduxComponents.Modules;
 
 public static class ExportedMekToolsComponentsAssemblies
 {
-  // Index Page
-  private static readonly Assembly IndexPageAssembly = typeof(IndexPage).Assembly;
+  // Dashboard Page
+  private static readonly Assembly DashboardAssembly = typeof(MainPage).Assembly;
 
-  // Dummy Page
-  private static readonly Assembly DummyPageAssembly = typeof(MainPage).Assembly;
-
-
-  public static Assembly[] All = new[] { IndexPageAssembly, DummyPageAssembly };
+  public static Assembly[] All = new[] { DashboardAssembly };
 }
