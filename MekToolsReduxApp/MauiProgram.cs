76b23f2a43b4eef12f1c05d274169778b78ba4a3
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-29 15:59:55 +0100

    feat (App) add MudBlazor dependency

--- MekToolsReduxApp/MauiProgram.cs
+++ MekToolsReduxApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace MekToolsReduxApp
 {
@@ -15,6 +16,7 @@ namespace MekToolsReduxApp
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
