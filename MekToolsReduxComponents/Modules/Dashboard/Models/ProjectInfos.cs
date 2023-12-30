c58f36f0619ad669de981b39648f211ede3e58db
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-30 21:32:17 +0100

    feat (DashBoardPage) add project Info UI

---
+++ MekToolsReduxComponents/Modules/Dashboard/Models/ProjectInfos.cs
@@ -0,0 +1,12 @@
+﻿namespace MekToolsReduxComponents.Modules.Dashboard.Models;
+
+public record ProjectInfos
+{
+  public string ProjectPath { get; set; } = null!;
+
+  public string? ModulesPath { get; set; }
+
+  public string ProjectName { get; set; } = null!;
+
+  public string CsProjName => $"{ProjectName}.csproj";
+}
