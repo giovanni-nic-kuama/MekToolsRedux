7ab967c4cbe2caa1748415e9e29b394315ed13f9
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-30 18:48:40 +0100

    feat (App) remove old components
    feat (App) add DashboardPage
    
    feat (DashBaord) add settings UI

---
+++ MekToolsReduxComponents/Modules/Dashboard/Models/ProjectSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MekToolsReduxComponents.Modules.Dashboard.Models;
+
+public record ProjectSettings
+{
+  public bool IsUpsertDtoEnabled { get; set; }
+
+  public bool IsRepositoryEnabled { get; set; }
+
+  public bool AreValidatorsEnabled { get; set; }
+
+  public bool IsEntityConfigurationEnabled { get; set; }
+}
--- MekToolsReduxComponents/Modules/ExportedMekToolsComponentsAssemblies.cs
+++ MekToolsReduxComponents/Modules/ExportedMekToolsComponentsAssemblies.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
-using MekToolsReduxComponents.Modules.Main.Pages;
+using MekToolsReduxComponents.Modules.Dashboard.Pages;
 
 namespace MekToolsReduxComponents.Modules;
 
 public static class ExportedMekToolsComponentsAssemblies
 {
   // Dashboard Page
-  private static readonly Assembly DashboardAssembly = typeof(MainPage).Assembly;
+  private static readonly Assembly DashboardAssembly = typeof(DashboardPage).Assembly;
 
-  public static Assembly[] All = new[] { DashboardAssembly };
+  public static readonly Assembly[] All = { DashboardAssembly };
 }
