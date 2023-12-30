aac68915cbd81997027c0623f3bdbe59ece93e3f
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-30 22:16:46 +0100

    feat (App) add project path input

---
+++ MekToolsReduxComponents/Modules/Dashboard/Forms/ProjectPathFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MekToolsReduxComponents.Modules.Dashboard.Forms;
+
+[Serializable]
+public class ProjectPathFormModel
+{
+  [Required(ErrorMessage = "Destination path is required")]
+  public string ProjectPath { get; set; } = null!;
+}
