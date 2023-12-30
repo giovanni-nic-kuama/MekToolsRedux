62aafb190b3b5aa127d2a3ecaa223b5cfd3083fe
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-30 21:32:17 +0100

    feat (DashBoardPage) add project Info UI

--- MekToolsReduxComponents/Modules/Dashboard/Pages/DashboardPage.razor.cs
+++ MekToolsReduxComponents/Modules/Dashboard/Pages/DashboardPage.razor.cs
@@ -6,10 +6,35 @@ public partial class DashboardPage
 {
   private ProjectSettings ProjectSettings { get; set; } = new();
 
+  private ProjectInfos? ProjectInfos { get; set; }
+
+  protected override void OnInitialized()
+  {
+    InitializeProjectInfos();
+
+    base.OnInitialized();
+  }
+
+  // TODO: delete
+  private void InitializeProjectInfos()
+  {
+    ProjectInfos = new ProjectInfos
+    {
+      ModulesPath = "C:\\Lavoro\\MekToolsRedux\\MekToolsReduxCore\\Modules",
+      ProjectPath = "C:\\Lavoro\\MekToolsRedux\\MekToolsReduxCore",
+      ProjectName = "MekToolsReduxCore"
+    };
+  }
 
   private void OnProjectSettingsChange(ProjectSettings value)
   {
     Console.WriteLine(value);
     ProjectSettings = value;
+  }
+
+  private void UnloadProject()
+  {
+    ProjectInfos = null;
+    StateHasChanged();
   }
 }
