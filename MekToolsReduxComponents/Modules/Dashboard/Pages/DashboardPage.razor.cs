6d238ef5e15ae6790cc62613a14b1fe91aed1cd9
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2023-12-30 18:48:40 +0100

    feat (App) remove old components
    feat (App) add DashboardPage
    
    feat (DashBaord) add settings UI

---
+++ MekToolsReduxComponents/Modules/Dashboard/Pages/DashboardPage.razor.cs
@@ -0,0 +1,15 @@
+﻿using MekToolsReduxComponents.Modules.Dashboard.Models;
+
+namespace MekToolsReduxComponents.Modules.Dashboard.Pages;
+
+public partial class DashboardPage
+{
+  private ProjectSettings ProjectSettings { get; set; } = new();
+
+
+  private void OnProjectSettingsChange(ProjectSettings value)
+  {
+    Console.WriteLine(value);
+    ProjectSettings = value;
+  }
+}
