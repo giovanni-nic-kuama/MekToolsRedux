2b140025e0fe75afbeb736bd17edbd298a80ffaa
Giovanni Nicoletti <77494407+giovanni-nic-kuama@users.noreply.github.com>
2024-01-27 12:38:38 +0100

    feat (EntityNameForm) add

---
+++ MekToolsReduxComponents/Modules/Dashboard/Forms/EntityNameFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MekToolsReduxComponents.Modules.Dashboard.Forms;
+
+[Serializable]
+public class EntityNameFormModel
+{
+  [Required(ErrorMessage = "Entity singular name is required")]
+  [RegularExpression(pattern:"/^[A-Z][a-z0-9_-]{3,19}$/", ErrorMessage = "Entity singular name must start with uppercase character")]
+  public string EntitySingularName { get; set; } = "Temperature";
+  
+  [Required(ErrorMessage = "Entity plural name is required")]
+  [RegularExpression(pattern:"/[a-zA-Z]/", ErrorMessage = "Entity plural name must start with uppercase character")]
+  public string EntityPluralName { get; set; } = "Temperatures";
+}
---
+++ MekToolsReduxComponents/Modules/Dashboard/Models/ProjectFolder.cs
@@ -0,0 +1,8 @@
+﻿namespace MekToolsReduxComponents.Modules.Dashboard.Models;
+
+public class ProjectFolder
+{
+  public string Name { get; set; } = null!;
+
+  public bool IsGenerated { get; set; }
+}
